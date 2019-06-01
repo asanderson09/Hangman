63503a508a0fa50fd34866fbc7abfda81dd8fb8f
Anthony A <asanderson2009@gmail.com>
2019-06-01 03:18:57 -0400

    added array for letters and started array for sweets

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// Array of Sweets and Treats for game
+var gameWord = ["Cheesecake", "Oreos", "Cupcake", "Churros", "Lemonhead", "Sherbert", "Chocolate", "Muffin", "Macaron", "Cookies", "Marshmallows", "Applesauce", "Doughnut", "Cake", "Pie", "Twizzlers", "Gushers", "Reese's", "Snickers", "Smores" ]
+
+// Array of alphabet Letters
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
